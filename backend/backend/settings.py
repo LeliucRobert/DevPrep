e66df9160b8d7f4483412bb98758b48209e83f02
robi9.lel@gmail.com <147204863+LeliucRobert@users.noreply.github.com>
2024-08-20 17:59:23 +0300

    Deployment v2.09

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -169,8 +169,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWS_CREDENTIALS = True
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = ('REDIS_URL','redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = ('REDIS_URL','redis://localhost:6379/0')
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
